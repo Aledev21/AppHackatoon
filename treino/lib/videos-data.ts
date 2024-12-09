9a58af4681be879f246b3adcf5afc629ce4b71c5
Alessandro Assis Procopio <playcerbr@outlook.com>
2024-12-09 01:16:27 -0300

    fix video and not

--- treino/lib/videos-data.ts
+++ treino/lib/videos-data.ts
@@ -2,11 +2,12 @@ export const videosData = [
   {
     id: '1',
     title: 'Conheça Nossa História',
-    embedUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
+    embedUrl: 'https://www.youtube.com/embed/07jmtR-rTPU?si=Y8Jr2MfzSWz8ivqA',
   },
   {
     id: '2',
     title: 'Sustentabilidade na Vale',
-    embedUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
+    embedUrl: 'https://www.youtube.com/embed/07jmtR-rTPU?si=Y8Jr2MfzSWz8ivqA',
   },
 ];
+
